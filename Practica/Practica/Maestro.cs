88c0ae41e62026610091d798e064b2c3367ab466
sotyk123 <sotyk123@gmail.com>
2016-09-07 19:19:57 -0700

    Clase

---
+++ Practica/Practica/Maestro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+   public class Maestro:Persona
+    {
+        public int Sueldo { get; set; }
+        public string Materia { get; set; }
+
+    }
+}
--- Practica/Practica/Program.cs
+++ Practica/Practica/Program.cs
@@ -10,6 +10,19 @@ namespace Practica
     {
         static void Main(string[] args)
         {
+            Alumno _alumno = new Alumno();
+            Maestro _maestro = new Maestro();
+
+            _alumno.Edad = 17;
+            _alumno.Nombre = "Arturo";
+            _alumno.NoControl = 14302060;
+            _maestro.Edad = 23;
+            _maestro.Nombre = "Christopher";
+            _maestro.Sueldo = 5000;
+
+            Console.WriteLine("La Edad Del Alumno Es: {0} su nombre es: {1} y NoControl: {2}", _alumno.Edad,_alumno.Nombre, _alumno.NoControl);
+            Console.WriteLine("La Edad Del Maestro Es: {0} su nombre es: {1} y Su Sueldo: {2}", _maestro.Edad, _maestro.Nombre, _maestro.Sueldo);
+            Console.ReadKey();
         }
     }
 }
